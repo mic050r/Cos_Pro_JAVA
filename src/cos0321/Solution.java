fafcd13b5c60f45df54bea3796b18a521a6dd1d2
mic050r <s2216@e-mirim.hs.kr>
2023-03-21 17:12:24 +0900

    0321 두 날짜에 며칠만큼 떨어져 있는지 구하는 문제

---
+++ src/cos0321/Solution.java
@@ -0,0 +1,31 @@
+package cos0321;
+
+class Solution {
+	public int func_a(int month, int day) {
+		int monthList[] = { 31, 28, 30, 31, 30, 31, 31, 30, 31, 30, 31 };
+		int total = 0;
+		for (int i = 0; i < (month - 1); i++) {
+			total += monthList[i];
+		}
+		total += day;
+		// 경과된 날이 있기 때문에 -1를 해줘야 함
+		return total - 1;
+	}
+
+	public int solution(int startMonth, int startDay, int endMonth, int endDay) {
+		int startTotal = func_a(startMonth, startDay);
+		int endTotal = func_a(endMonth, endDay);
+		return endTotal - startTotal;
+	}
+
+	public static void main(String[] args) {
+		Solution sol = new Solution();
+		int startMonth = 1;
+		int startDay = 2;
+		int endMonth = 2;
+		int endDay = 2;
+		int ret = sol.solution(startMonth, startDay, endMonth, endDay);
+		System.out.println("Solution : return value of the method is " + ret + ".");
+	}
+
+}
