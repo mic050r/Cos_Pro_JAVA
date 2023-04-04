2e4dec22d8b03a2dfba38604bb3249a7220a5d6f
mic050r <s2216@e-mirim.hs.kr>
2023-04-05 01:56:36 +0900

    0330 연속하는 중복 문자를 삭제하는 문제

--- src/cos0330/Solution.java
+++ src/cos0330/Solution.java
@@ -5,6 +5,7 @@ class Solution {
 		String result = "";
 		result += characters.charAt(0);
 		// 배열이면 .length 스트링이면 .length()
+		// 배열은 0부터 시작이라서 characters 길이에 -1 해주기
 		for (int i = 0; i < characters.length() - 1; i++) {
 			if (characters.charAt(i) != characters.charAt(i + 1)) {
 				result += characters.charAt(i + 1);
