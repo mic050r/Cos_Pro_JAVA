8348325cbf1f4b60dd41b44672dd7353fd8d08d4
mic050r <103114387+mic050r@users.noreply.github.com>
2023-06-09 03:30:07 +0900

    0601 XX 시험을 친 수험생들의 점수가 주어질 때, 합격자 수를 구하는 문제

---
+++ src/cos0601/Solution.java
@@ -0,0 +1,22 @@
+package cos0601;
+
+public class Solution {
+	public int solution(int[] scores, int cutline) {
+		int answer = 0;
+		for (int i = 0; i < scores.length; i++) {
+			if (scores[i] >= cutline) {
+				answer++;
+			}
+		}
+		return answer;
+	}
+
+	public static void main(String[] args) {
+		Solution sol = new Solution();
+		int[] scores = { 80, 90, 55, 60, 59 };
+		int cutline = 60;
+		int ret = sol.solution(scores, cutline);
+
+		System.out.println("solution 메소드의 반환 값은 " + ret + " 입니다.");
+	}
+}
